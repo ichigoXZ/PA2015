9cccb8cf19d5a8a26f0223c3a6fe272985c923f2
tracer-ics2015 <tracer@njuics.org>
2015-09-23 19:20:00 +0800

    >  compile NEMU 141220000 ting Linux ele 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  19:20:00 up 29 min,  3 users,  load average: 0.52, 0.40, 0.33 9061379d8c4109e95dc66a00d31779415bf3a96c

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -107,7 +107,9 @@ void ui_mainloop() {
 		int i;
 		for(i = 0; i < NR_CMD; i ++) {
 			if(strcmp(cmd, cmd_table[i].name) == 0) {
-				if(cmd_table[i].handler(args) < 0) { return; }
+				if(cmd_table[i].handler(args) < 0) {
+                        printf("%s",args);
+                        return; }
 				break;
 			}
 		}
