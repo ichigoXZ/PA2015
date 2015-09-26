fbefd2738240191581407cb552c8679c31f9976f
tracer-ics2015 <tracer@njuics.org>
2015-09-26 23:02:40 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  23:02:40 up 12:36,  3 users,  load average: 0.07, 0.18, 0.26 a8488de21a1cccc32f1e46bfec4699bd746ad26b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -192,8 +192,9 @@ uint32_t dominant_operator(uint32_t p,uint32_t q){
 		printf("%d\t%d\n",stack[j].op,stack[j].position);
 	printf("i-1=%d\n",i);
 	for(j = i-1 ;j >= 0;j--)
-		if(stack[j].op==1)
-			return stack[j].position;
+		if(stack[j].op==1){
+			printf("iii\n");
+			return stack[j].position;}
 	return stack[i-1].position;
 }
 
