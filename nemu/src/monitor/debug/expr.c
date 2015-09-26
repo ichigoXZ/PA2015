775f06574a43019431893357aab4b89742fc76d3
tracer-ics2015 <tracer@njuics.org>
2015-09-26 23:10:55 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  23:10:55 up 12:44,  3 users,  load average: 0.52, 0.23, 0.24 fc6a269a54b4fad731ab8673b6653f4d1b631ff3

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -166,8 +166,7 @@ uint32_t eval(uint32_t p,uint32_t q){
 
 uint32_t dominant_operator(uint32_t p,uint32_t q){
 	uint32_t pos = p;
-	uint32_t i = 0;
-	uint32_t j = 0;
+	uint32_t i = 0,j = 0;
 	typedef struct mark{
 		uint32_t position;
 		uint32_t op;
@@ -187,15 +186,13 @@ uint32_t dominant_operator(uint32_t p,uint32_t q){
 			case DIVIDE:stack[i].position = pos;
 						stack[i++].op = 0; 
 						break;
+			case LEFT_R: {pos++;}while(tokens[pos].type != RIGHT_R);
+						break;
 			default: break;
 			}
-	for(j=0;j<i;j++)
-		printf("%d\t%d\n",stack[j].op,stack[j].position);
-	printf("i-1=%d\n",i);
-	for(j=i-1;j>=0;j--)
-		if(stack[j].op==1){
-			printf("ii %d i\n",j);
-			return stack[j].position;}
+	for(j = i-1 ;j >= 0;j--)
+		if(stack[j].op==1)
+			return stack[j].position;
 	return stack[i-1].position;
 }
 
