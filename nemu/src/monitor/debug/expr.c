005d40be555f6b2c48083b058b92591fb53c2571
tracer-ics2015 <tracer@njuics.org>
2015-09-28 14:39:16 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  14:39:16 up  1:53,  4 users,  load average: 0.47, 0.27, 0.23 ca7e7c74fd697d7f469d88b196512ebc76a2ed1f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -155,6 +155,7 @@ uint32_t eval(uint32_t p,uint32_t q){
 		else if(tokens[p].type == HEX_NUM)
 			return (uint32_t)strtol(tokens[p].str,NULL,16);
 		else if(tokens[p].type == REG){
+        printf("eax: %s\n",tokens[p].str+1);
 			if(strcmp(tokens[p].str,"$eax")){
         printf("eax: 0x%.8x\n",cpu.eax);
 				return cpu.eax;}
