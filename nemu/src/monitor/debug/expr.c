269c419261d62088517df21039e218aef7f2764d
ichigoXZ <atathunder@gmail.com>
2015-09-28 14:37:22 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  14:37:22 up  1:52,  4 users,  load average: 0.20, 0.16, 0.19 ef1aff3a9f06817d8ec54629acc0fcb92a4d9aaa

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -155,7 +155,6 @@ uint32_t eval(uint32_t p,uint32_t q){
 		else if(tokens[p].type == HEX_NUM)
 			return (uint32_t)strtol(tokens[p].str,NULL,16);
 		else if(tokens[p].type == REG){
-        printf("eax: 0x%.8x\n",cpu.eax);
 			if(strcmp(tokens[p].str,"$eax")){
         printf("eax: 0x%.8x\n",cpu.eax);
 				return cpu.eax;}
