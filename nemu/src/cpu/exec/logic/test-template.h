6722192c2020f61c0fcbdd6997d29130a291c620
ichigoXZ <atathunder@gmail.com>
2015-10-29 23:01:12 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  23:01:12 up  7:14,  2 users,  load average: 1.31, 1.07, 0.94 ecd234a2c1d40a7de4cf005a5667a83265fbe452

---
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -0,0 +1,39 @@
+#include "cpu/exec/template-start.h"
+
+#define instr test
+
+static void do_execute () {
+	DATA_TYPE result = op_dest->val & op_src->val;
+
+	/* TODO: Update EFLAGS. */
+	cpu.Eflags.CF = 0;
+	cpu.Eflags.OF = 0;
+	if(result == 0)
+		cpu.Eflags.ZF = 1;
+		else 
+		cpu.Eflags.ZF = 0;
+#if DATA_TYPE == 1
+		if(result&0x80)
+			cpu.Eflags.SF = 0;
+			else 
+			cpu.Eflags.SF = 1;
+#elif DATA_TYPE == 2
+		if(result&0x8000)
+			cpu.Eflags.SF = 0;
+			else 
+			cpu.Eflags.SF = 1;
+#else 
+		if(result&0x80000000)
+			cpu.Eflags.SF = 0;
+			else 
+			cpu.Eflags.SF = 1;
+#endif
+
+	print_asm_template2();
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+
+#include "cpu/exec/template-end.h"
