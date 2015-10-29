9afba0ae86b2b670a11548a2bbe5f3dd7d0e9c37
ichigoXZ <atathunder@gmail.com>
2015-10-29 20:41:47 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  20:41:47 up  4:55,  2 users,  load average: 1.09, 0.97, 0.92 d486c6e6506504ec89e705f4f3a7f331820360fe

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -10,6 +10,7 @@
 #include "arith/mul.h"
 #include "arith/idiv.h"
 #include "arith/div.h"
+//#include "arith/sub.h"
 
 #include "logic/and.h"
 #include "logic/or.h"
@@ -25,3 +26,5 @@
 #include "misc/misc.h"
 
 #include "special/special.h"
+
+//#include "jump/je.h"
