2234064cd14f46645166078367cd739f68b5bbf4
tracer-ics2015 <tracer@njuics.org>
2015-10-29 23:24:52 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  23:24:52 up  7:38,  2 users,  load average: 0.71, 0.74, 0.78 d4cb8c2b1aef478af7ca9d99e90ab427947cdef3

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -4,7 +4,7 @@
 
 static void do_execute () {
 		DATA_TYPE result = op_dest->val - op_src->val;
-			//OPERAND_W(op_dest, result);
+		OPERAND_W(op_dest, result);
 
 				/* TODO: Update EFLAGS. */
 		if(result == 0)
@@ -48,8 +48,11 @@ static void do_execute () {
 }
 
 make_instr_helper(i2a)
+make_instr_helper(i2rm)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
-make_instr_helper(i2rm)
 
 #include "cpu/exec/template-end.h"
