44a98201d02cd7c1a3ddd0c7e8e438b23992c85e
ichigoXZ <atathunder@gmail.com>
2015-10-31 14:24:28 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  14:24:28 up  4:53,  2 users,  load average: 0.49, 0.60, 0.55 f8716af9cc53a7a7620ac6e0b7af5d966b152bd4

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -7,7 +7,15 @@ static void do_execute () {
 		OPERAND_W(op_dest, result);
 
 				/* TODO: Update EFLAGS. */
-		
+		if(result == 0)
+			cpu.Eflags.ZF = 1;
+			else 
+			cpu.Eflags.ZF = 0;
+		if(op_dest->val<op_src->val)
+			cpu.Eflags.CF = 1;
+			else 
+			cpu.Eflags.CF = 0;
+
 
 		print_asm_template2();
 }
