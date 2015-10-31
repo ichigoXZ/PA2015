b72736dc3005a028d9f3ddbe77b49d8eb6a18638
tracer-ics2015 <tracer@njuics.org>
2015-10-31 13:30:39 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  13:30:39 up  3:59,  2 users,  load average: 0.50, 0.57, 0.51 28f0daaf8cd7ebb50cad6e7e6527bef5e8d007e

--- nemu/src/cpu/exec/jump/call-template.h
+++ nemu/src/cpu/exec/jump/call-template.h
@@ -4,8 +4,10 @@
 
 static void do_execute () {
 #if DATA_BYTE == 2
+	MEM_R(cpu.eip);
 	cpu.eip = (cpu.eip + op_src->val) & 0x0000ffff;
 #elif DATA_BYTE == 4
+	MEM_R(cpu.eip);
 	cpu.eip = cpu.eip + op_src->val;
 #endif
 
