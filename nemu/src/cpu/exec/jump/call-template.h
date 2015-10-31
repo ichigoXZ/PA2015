0a36d968839db9ddfec52979e5205d984146b17f
ichigoXZ <atathunder@gmail.com>
2015-10-31 14:57:31 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  14:57:31 up  5:26,  3 users,  load average: 0.76, 0.70, 0.60 cfa60cf2cfd61043b77de13e2705e2b5fdb8efaf

--- nemu/src/cpu/exec/jump/call-template.h
+++ nemu/src/cpu/exec/jump/call-template.h
@@ -4,17 +4,7 @@
 
 static void do_execute () {
 	printf("%d\n",op_src->val);
-#if DATA_BYTE == 2
-	cpu.esp = cpu.esp - 2;
-	//实现段寄存器
-	MEM_W(cpu.esp, cpu.eip);
-	cpu.eip = (cpu.eip + op_src->val) & 0x0000ffff;
-#elif DATA_BYTE == 4
-	cpu.esp = cpu.esp - 4;
-	//实现段寄存器
-	MEM_W(cpu.esp, cpu.eip);
-	cpu.eip = cpu.eip + op_src->val;
-#endif
+
 
 	print_asm_template1();
 }
