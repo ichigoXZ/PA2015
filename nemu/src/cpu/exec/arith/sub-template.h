1bc478909d0893bb799fd7582fd9bc76fb97d636
ichigoXZ <atathunder@gmail.com>
2015-10-31 14:21:51 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  14:21:51 up  4:50,  2 users,  load average: 0.80, 0.63, 0.54 da489660b8ee85bc1fc86d7d3f6b9ad813eb7d4

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -7,42 +7,7 @@ static void do_execute () {
 		OPERAND_W(op_dest, result);
 
 				/* TODO: Update EFLAGS. */
-		if(result == 0)
-			cpu.Eflags.ZF = 1;
-			else 
-			cpu.Eflags.ZF = 0;
-		if(op_dest->val<op_src->val)
-			cpu.Eflags.CF = 1;
-			else 
-			cpu.Eflags.CF = 0;
-#if DATA_TYPE == 1
-		if(result&0x80)
-			cpu.Eflags.SF = 0;
-			else 
-			cpu.Eflags.SF = 1;
-		if(((op_dest->val&0x80)^(op_src->val&0x80))&&((op_dest->val&0x80)^(result&0x8000)))
-			cpu.Eflags.OF = 1;
-			else
-			cpu.Eflags.OF = 0;
-#elif DATA_TYPE == 2
-		if(result&0x8000)
-			cpu.Eflags.SF = 0;
-			else 
-			cpu.Eflags.SF = 1;
-		if(((op_dest->val&0x8000)^(op_src->val&0x8000))&&((op_dest->val&0x8000)^(result&0x8000)))
-			cpu.Eflags.OF = 1;
-			else
-			cpu.Eflags.OF = 0;
-#else 
-		if(result&0x80000000)
-			cpu.Eflags.SF = 0;
-			else 
-			cpu.Eflags.SF = 1;
-		if(((op_dest->val&0x80000000)^(op_src->val&0x80000000))&&((op_dest->val&0x80000000)^(result&0x8000)))
-			cpu.Eflags.OF = 1;
-			else
-			cpu.Eflags.OF = 0;
-#endif
+		
 
 		print_asm_template2();
 }
