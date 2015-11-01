b06eba2babb347d5af7107c1285ed3fb05512881
tracer-ics2015 <tracer@njuics.org>
2015-11-01 22:35:10 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  22:35:10 up 11:55,  3 users,  load average: 1.23, 1.26, 0.83 5ae29360abaf02d716cf7387e49b950ad88baf48

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -59,8 +59,7 @@ static int cmd_info(char *args) {
         printf("edi: 0x%.8x\n",cpu.edi);
         printf("eip: 0x%.8x\n",cpu.eip);
         printf("Eflags: 0x%.8x\n",cpu.Eflags);
-        printf("%d\n",cpu.a1 );
-    }
+          }
 	else if('w' == *args){
 		WP *p = head;
 		for( ;p != NULL;p = p->next)
