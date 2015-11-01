41b274a0d0c7b0fed8d60355b92b7d17283d4deb
tracer-ics2015 <tracer@njuics.org>
2015-11-01 21:38:51 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  21:38:51 up 10:59,  4 users,  load average: 0.86, 0.81, 0.73 f7b5dad4c3b1102421e6e031118fb76504b6f03

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -138,6 +138,7 @@ static bool make_token(char *e) {
 							  break;
 					case VAR: tokens[nr_token].type = VAR;
 							  strncpy(tokens[nr_token].str,substr_start,substr_len);
+							  *(tokens[nr_token].str+substr_len)='\0';
 							  nr_token++;
 							  break;
 					default: panic("please implement me");
