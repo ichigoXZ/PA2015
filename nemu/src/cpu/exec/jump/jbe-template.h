72b5a6980f1c8d69a95c7c9c3f46fca6ee5e0e44
ichigoXZ <atathunder@gmail.com>
2015-11-01 16:52:19 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  16:52:19 up  6:55,  3 users,  load average: 1.08, 0.89, 0.75 c5056646cce7de926fa9b8e0b8b4b850405dc149

--- nemu/src/cpu/exec/jump/jbe-template.h
+++ nemu/src/cpu/exec/jump/jbe-template.h
@@ -4,7 +4,17 @@
 
 static void do_execute () {
 	if(cpu.Eflags.CF==1 || cpu.Eflags.ZF==1){
-
+#if DATA_TYPE == 1
+		uint8_t val = (uint8_t)(cpu.eip&0xff);
+		val += (uint8_t)op_src->val;
+		cpu.eip=(cpu.eip&0xffffff00)+val;
+#elif DATA_TYPE == 2
+		uint16_t val = (uint16_t)(cpu.eip&0xffff);
+		val += (uint16_t)op_src->val;
+		cpu.eip=(cpu.eip&0xffff0000)+val;
+#else
+		cpu.eip=cpu.eip+(int32_t)op_src->val;
+#endif
 	}
 
 	print_asm_template1();
