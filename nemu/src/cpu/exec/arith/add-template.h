83286f6da7e009fb9850c788f151a3beafbda025
tracer-ics2015 <tracer@njuics.org>
2015-11-01 10:26:27 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  10:26:27 up 30 min,  3 users,  load average: 1.44, 1.19, 0.99 35ef813d433af265c953ddc84e8a8e7b32efb919

---
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -0,0 +1,57 @@
+#include "cpu/exec/template-start.h"
+
+#define instr add
+
+static void do_execute () {
+		DATA_TYPE result = op_dest->val + op_src->val;
+		OPERAND_W(op_dest, result);
+				/* TODO: Update EFLAGS. */
+	if(result == 0)
+			cpu.Eflags.ZF = 1;
+			else 
+			cpu.Eflags.ZF = 0;
+	if(result < op_dest->val || result < op_src->val)
+			cpu.Eflags.CF = 1;
+			else 
+			cpu.Eflags.CF = 0;
+#if DATA_TYPE == 1
+		if(result&0x80)
+			cpu.Eflags.SF = 1;
+			else 
+			cpu.Eflags.SF = 0;
+		if(!((op_dest->val&0x80)^(op_src->val&0x80))&&((op_dest->val&0x80)^(result&0x8000)))
+			cpu.Eflags.OF = 1;
+			else
+			cpu.Eflags.OF = 0;
+#elif DATA_TYPE == 2
+		if(result&0x8000)
+			cpu.Eflags.SF = 1;
+			else 
+			cpu.Eflags.SF = 0;
+		if(!((op_dest->val&0x8000)^(op_src->val&0x8000))&&((op_dest->val&0x8000)^(result&0x8000)))
+			cpu.Eflags.OF = 1;
+			else
+			cpu.Eflags.OF = 0;
+#else 
+		if(result&0x80000000)
+			cpu.Eflags.SF = 1;
+			else 
+			cpu.Eflags.SF = 0;
+		if(!((op_dest->val&0x80000000)^(op_src->val&0x80000000))&&((op_dest->val&0x80000000)^(result&0x8000)))
+			cpu.Eflags.OF = 1;
+			else
+			cpu.Eflags.OF = 0;
+#endif
+
+		print_asm_template2();
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
