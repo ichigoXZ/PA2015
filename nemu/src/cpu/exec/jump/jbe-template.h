c35cbfa16f43ac98af508848993553ef7a62a01e
tracer-ics2015 <tracer@njuics.org>
2015-11-02 15:40:57 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  15:40:57 up  1:38,  2 users,  load average: 0.71, 0.73, 0.70 f87936f781b6d4c8e6d7ec6fecf3df5ac998d16

--- nemu/src/cpu/exec/jump/jbe-template.h
+++ nemu/src/cpu/exec/jump/jbe-template.h
@@ -6,7 +6,9 @@ static void do_execute () {
 	if(cpu.CF || cpu.ZF){
 		cpu.eip = cpu.eip + op_src->val;
 		printf("%d\n",DATA_BYTE );
-#if DATA_BYTE == 2 
+#if DATA_BYTE == 1
+		cpu.eip = cpu.eip & 0x000000ff;
+#elif DATA_BYTE == 2 
 		cpu.eip = cpu.eip & 0x0000ffff;
 #endif
 	}
