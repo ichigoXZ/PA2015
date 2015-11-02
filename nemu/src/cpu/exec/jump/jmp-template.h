72f3928ad32b8414f5235b2ea6563ae924840b9a
ichigoXZ <atathunder@gmail.com>
2015-11-02 14:16:19 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  14:16:19 up 13 min,  2 users,  load average: 1.04, 0.88, 0.64 b42deab9a9355b4a3802f89164bd1beb7e6d290e

--- nemu/src/cpu/exec/jump/jmp-template.h
+++ nemu/src/cpu/exec/jump/jmp-template.h
@@ -5,7 +5,7 @@
 static void do_execute () {
 	cpu.eip += op_src->val;
 #if DATA_BYTE == 2
-	cpu.eip = (cpu.eip + op_src->val) & 0x0000ffff;
+	cpu.eip = cpu.eip & 0x0000ffff;
 #endif
 
 	print_asm_template1();
