b490e4228dc892ee91005c9a29fd18e0b87ee749
tracer-ics2015 <tracer@njuics.org>
2015-11-03 10:20:02 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  10:20:02 up  1:57,  2 users,  load average: 0.88, 0.71, 0.63 12336087bc73de9fda8a7a422b56bbddd8e0be3d

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -132,7 +132,7 @@ static int cmd_bt(char *args){
 		p = (PartOFStackFrame*)malloc(sizeof(PartOFStackFrame));
 		p->prev_ebp = swaddr_read(q->prev_ebp,4);
 		p->ret_addr = swaddr_read(q->prev_ebp+4,4);
-		printf("%d\t0x%x\t0x%x\n",no++,q->prev_ebp,p->prev_ebp );
+		printf("#%d\t0x%x\t0x%x\n",no++,p->prev_ebp,p->ret_addr );
 		q = p;
 	}
 	return 0;
