526ee9c19b1804e9aa23ebdaa305791da86fe311
ichigoXZ <atathunder@gmail.com>
2015-11-03 08:52:40 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  08:52:40 up 29 min,  2 users,  load average: 0.92, 0.62, 0.54 d768d3f5f230ac2ced68c290646c0c0d2e7be49c

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -119,18 +119,18 @@ static int cmd_d(char *args){
 }
 
 static int cmd_bt(char *args){
-	int no=0,i;
+
 	//PartOFStackFrame psf;
 	//psf.prev_ebp = swaddr_read(cpu.ebp,4);
 	printf("%d\n",cpu.ebp);
 	printf("%d\n",swaddr_read(cpu.eip,4));	
-		for(i=0;i<nr_symtab_entry;i++)
+		/*for(i=0;i<nr_symtab_entry;i++)
 		{
 				printf("#%d\t%d\t%s\n",no++,(symtab+i)->st_value,&strtab[(symtab+i)->st_name]);
 				//psf.ret_addr = swaddr_read(cpu.eip,4);
 			}
 		if(i == nr_symtab_entry)
-			printf("no stack\n");
+			printf("no stack\n");*/
 	return 0;
 }
 
