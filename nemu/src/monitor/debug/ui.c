2800dfdbe1659d77841860365c63656cada675e8
ichigoXZ <atathunder@gmail.com>
2015-11-03 10:50:15 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  10:50:15 up  2:27,  2 users,  load average: 0.52, 0.63, 0.60 1426ade9677440cd37422eaa40d3f3ffd0359c2

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -136,6 +136,7 @@ static int cmd_bt(char *args){
 		p->prev_ebp = swaddr_read(past->prev_ebp,4);
 		//p->ret_addr = swaddr_read(q->prev_ebp+4,4)+decode_i_l(q->prev_ebp)+1;
 		printf("#%d\t0x%x\t0x%x\n",no++,past->prev_ebp,past->ret_addr );
+		printf("#%d\t0x%x\t0x%x\n",no++,cpu.ebp,past->ret_addr );
 		for( ;i<nr_symtab_entry;i++){
 			if(18 == (symtab+i)->st_info){
 				if(cpu.ebp == (symtab+i)->st_value)
