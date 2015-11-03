204be8ecb2489f78d059fc3b648c7ea400a33c50
tracer-ics2015 <tracer@njuics.org>
2015-11-03 08:55:13 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  08:55:13 up 32 min,  2 users,  load average: 0.64, 0.60, 0.54 acbb3ebcb294b258109d0723d0da61f6867b5995

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -123,7 +123,7 @@ static int cmd_bt(char *args){
 	//PartOFStackFrame psf;
 	//psf.prev_ebp = swaddr_read(cpu.ebp,4);
 	printf("%x\n",cpu.ebp);
-	printf("%x\n",swaddr_read(cpu.eip,4));	
+	printf("%x\n",swaddr_read(cpu.ebp,4));	
 		/*for(i=0;i<nr_symtab_entry;i++)
 		{
 				printf("#%d\t%d\t%s\n",no++,(symtab+i)->st_value,&strtab[(symtab+i)->st_name]);
