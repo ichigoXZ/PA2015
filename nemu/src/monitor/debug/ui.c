cfa75bcfe805c6a967acfcf25b3d3e9138b412a3
tracer-ics2015 <tracer@njuics.org>
2015-11-03 09:05:32 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  09:05:32 up 42 min,  2 users,  load average: 0.78, 0.53, 0.50 d9096c9293bbc56b7840e9b351125df749dbf9e

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -119,16 +119,16 @@ static int cmd_d(char *args){
 }
 
 static int cmd_bt(char *args){
-
+	int no = 0,i;
 	//PartOFStackFrame psf;
 	//psf.prev_ebp = swaddr_read(cpu.ebp,4);
 	printf("%x\n",cpu.ebp);
 	printf("%x\n",swaddr_read(cpu.ebp,4));	
-		/*for(i=0;i<nr_symtab_entry;i++)
+		for(i=0;i<nr_symtab_entry;i++)
 		{
 				printf("#%d\t%d\t%s\n",no++,(symtab+i)->st_value,&strtab[(symtab+i)->st_name]);
 				//psf.ret_addr = swaddr_read(cpu.eip,4);
-			}
+			}/*
 		if(i == nr_symtab_entry)
 			printf("no stack\n");*/
 	return 0;
