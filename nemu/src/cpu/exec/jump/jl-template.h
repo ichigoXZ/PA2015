1e5da9f28f1feed55d7b7cf16e9f317b1750626e
ichigoXZ <atathunder@gmail.com>
2015-11-05 20:15:56 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  20:15:56 up  1:19,  2 users,  load average: 0.57, 0.74, 0.64 1a5bbaaa24429a7c4faf5a7740f0017bb2003765

---
+++ nemu/src/cpu/exec/jump/jl-template.h
@@ -0,0 +1,25 @@
+#include "cpu/exec/template-start.h"
+
+#define instr jl
+
+static void do_execute () {
+	if(cpu.SF != cpu.OF){
+#if DATA_BYTE == 1
+		if(op_src->val&0x80){
+			uint32_t val = 0xffffff00 | op_src->val;
+			cpu.eip += val;
+		}
+		else 
+			cpu.eip +=op_src->val;
+#elif DATA_BYTE == 2 
+			cpu.eip += op_src->val;
+		cpu.eip = cpu.eip & 0x0000ffff;
+#endif
+	}
+
+	print_asm_template1();
+}
+
+make_instr_helper(i)
+
+#include "cpu/exec/template-end.h"
