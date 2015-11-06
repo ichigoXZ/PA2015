4b09994a8496e746bdf2f800a694857d0be8a2c7
tracer-ics2015 <tracer@njuics.org>
2015-11-06 20:26:20 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  20:26:20 up  4:11,  2 users,  load average: 0.18, 0.30, 0.39 1b180f3942d3dfea3a02486ad9c7ef0b81cbae12

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -3,6 +3,7 @@
 #define instr test
 
 static void do_execute () {
+	printf("src: %x  dest: %x\n",op_src->val,op_dest->val );
 	DATA_TYPE result = op_src->val & op_dest->val;
 	//OPERAND_W(op_dest, result);
 	/* TODO: Update EFLAGS. */
