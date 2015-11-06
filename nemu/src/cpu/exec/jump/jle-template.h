9f6912f3b04b29aabeae9d1e30ae44490c7a1700
tracer-ics2015 <tracer@njuics.org>
2015-11-06 21:30:23 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  21:30:23 up  5:15,  2 users,  load average: 0.38, 0.41, 0.32 2490bbfe197267bb32f1ab13fea4dab87b2acb4c

--- nemu/src/cpu/exec/jump/jle-template.h
+++ nemu/src/cpu/exec/jump/jle-template.h
@@ -3,16 +3,9 @@
 #define instr jle
 
 static void do_execute () {
-	if(cpu.ZF && cpu.SF != cpu.OF){
-#if DATA_BYTE == 1
-		if(op_src->val&0x80){
-			uint32_t val = 0xffffff00 | op_src->val;
-			cpu.eip += val;
-		}
-		else 
+	if(cpu.ZF && (cpu.SF != cpu.OF)){
 			cpu.eip +=op_src->val;
-#elif DATA_BYTE == 2 
-			cpu.eip += op_src->val;
+#if DATA_BYTE == 2 
 		cpu.eip = cpu.eip & 0x0000ffff;
 #endif
 	}
