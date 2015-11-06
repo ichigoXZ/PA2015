335270ccb6d26cfc74e34f77469fb1c402ca3095
tracer-ics2015 <tracer@njuics.org>
2015-11-06 21:41:33 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  21:41:33 up  5:26,  2 users,  load average: 0.86, 0.65, 0.47 ef4e976bcfdf73cc95daf6dddc881819e1514014

--- nemu/src/cpu/exec/jump/jge-template.h
+++ nemu/src/cpu/exec/jump/jge-template.h
@@ -4,15 +4,8 @@
 
 static void do_execute () {
 	if(cpu.SF == cpu.OF){
-#if DATA_BYTE == 1
-		if(op_src->val&0x80){
-			uint32_t val = 0xffffff00 | op_src->val;
-			cpu.eip += val;
-		}
-		else 
 			cpu.eip +=op_src->val;
-#elif DATA_BYTE == 2 
-			cpu.eip += op_src->val;
+#if DATA_BYTE == 2 
 		cpu.eip = cpu.eip & 0x0000ffff;
 #endif
 	}
