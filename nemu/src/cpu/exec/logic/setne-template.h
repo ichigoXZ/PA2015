f591566dab0dd71edb9f1c5bd086a07bc3a2a114
ichigoXZ <atathunder@gmail.com>
2015-11-06 20:25:25 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  20:25:25 up  4:10,  2 users,  load average: 0.45, 0.36, 0.42 3c665afc8c619992441c0660e11b5ad6ca90285d

--- nemu/src/cpu/exec/logic/setne-template.h
+++ nemu/src/cpu/exec/logic/setne-template.h
@@ -3,13 +3,11 @@
 #define instr setne
 
 static void do_execute() {
-	printf("setne: initial :op_dest->val%d\n",op_dest->val );
 	if(cpu.ZF==0)
 		op_dest->val = 1;
 	else
 		op_dest->val = 0;
 
-	printf("setne:op_dest->val%d\n",op_dest->val );
 	print_asm_template1();
 }
 
