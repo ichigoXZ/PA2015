91fc528613ef3dbc1062a4a361340e61c898b1fe
ichigoXZ <atathunder@gmail.com>
2015-11-06 21:25:40 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  21:25:40 up  5:10,  2 users,  load average: 0.25, 0.22, 0.24 2ffd0f3c55f0db1a3892a4b94e5e4520f81cbeb5

--- nemu/src/cpu/exec/jump/jle-template.h
+++ nemu/src/cpu/exec/jump/jle-template.h
@@ -3,7 +3,7 @@
 #define instr jle
 
 static void do_execute () {
-	if(cpu.ZF && cpu.SF != cpu.OF){
+	if(cpu.ZF && (cpu.SF != cpu.OF)){
 			cpu.eip +=op_src->val;
 #if DATA_BYTE == 2 
 		cpu.eip = cpu.eip & 0x0000ffff;
