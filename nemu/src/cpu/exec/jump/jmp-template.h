7fe8dcbbe706f3937d038198ffa7efca5aa37d0f
tracer-ics2015 <tracer@njuics.org>
2015-11-06 21:45:50 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  21:45:50 up  5:30,  2 users,  load average: 0.99, 0.86, 0.61 c32b01dd5da6ddca5e6ba84725f2e61d428103b8

--- nemu/src/cpu/exec/jump/jmp-template.h
+++ nemu/src/cpu/exec/jump/jmp-template.h
@@ -4,8 +4,16 @@
 
 static void do_execute () {
 	cpu.eip += op_src->val;
-#if DATA_BYTE == 2
-	cpu.eip = cpu.eip & 0x0000ffff;
+#if DATA_BYTE == 1
+		if(op_src->val&0x80){
+			uint32_t val = 0xffffff00 | op_src->val;
+			cpu.eip += val;
+		}
+		else 
+			cpu.eip +=op_src->val;
+#elif DATA_BYTE == 2 
+			cpu.eip += op_src->val;
+		cpu.eip = cpu.eip & 0x0000ffff;
 #endif
 
 	print_asm_template1();
