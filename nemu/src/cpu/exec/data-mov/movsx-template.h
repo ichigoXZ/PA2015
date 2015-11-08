7a61f00529cba7318222a8fc14a352dba6acef49
ichigoXZ <atathunder@gmail.com>
2015-11-08 18:42:09 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  18:42:09 up 26 min,  2 users,  load average: 1.02, 1.03, 0.87 dc9871f9591562734608c3c6f85d4c4b41ef3515

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -11,8 +11,8 @@ static void do_execute() {
 		else 
 	OPERAND_W(op_dest, op_src->val);
 #elif DATA_BYTE == 2 
-			if(op_src->val&0x8000){
-			uint32_t val = 0xffff0000 | op_src->val;
+			if(op_src->val&0x800){
+			uint32_t val = 0xffffff00 | op_src->val;
 			OPERAND_W(op_dest,val);
 		}
 		else 
