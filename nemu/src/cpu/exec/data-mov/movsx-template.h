7eebdd0e185e30013a77286e0beba4be3563c9eb
tracer-ics2015 <tracer@njuics.org>
2015-11-08 18:58:45 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  18:58:45 up 42 min,  2 users,  load average: 0.61, 0.62, 0.71 8ada4464cebc20045282cc4612f0ef5e2fc3a6

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -4,20 +4,14 @@
 
 static void do_execute() {
 #if DATA_BYTE == 1
-	//printf("DATA_BYTE == 1\n");
 		if(op_src->val&0x80){
-			printf("ini:%x\n",op_src->val );
 			uint32_t val = 0xffffff00 | op_src->val;
-			printf("%x\n",val );
-			OPERAND_W(op_dest,op_src->val);
-			printf("op_src:%x\n",op_dest->val );
 			OPERAND_W(op_dest,val);
-			printf("val--dest:%x\n",op_dest->val);
+			cpu.eax |= 0xffffff00;
 		}
 		else 
 	OPERAND_W(op_dest, op_src->val);
 #elif DATA_BYTE == 2 
-	//printf("DATA_BYTE == 2\n");
 			if(op_src->val&0x8000){
 			uint32_t val = 0xffff0000 | op_src->val;
 			OPERAND_W(op_dest,val);
@@ -25,7 +19,6 @@ static void do_execute() {
 		else 
 	OPERAND_W(op_dest, op_src->val);
 #else
-	//printf("DATA_BYTE == 4\n");
 	OPERAND_W(op_dest, op_src->val);
 #endif
 
