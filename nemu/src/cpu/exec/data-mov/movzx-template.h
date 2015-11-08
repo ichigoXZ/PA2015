9b100b7fd81d78bfaf69cfae6c0baf0cc789cfcc
ichigoXZ <atathunder@gmail.com>
2015-11-08 18:45:17 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  18:45:17 up 29 min,  2 users,  load average: 0.67, 0.92, 0.86 a22bd756251911df74cea3397e2a9d6bdd93eba

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -4,12 +4,15 @@
 
 static void do_execute() {
 #if DATA_BYTE == 1	
+	printf("DATA_BYTE == 1\n");
 	uint32_t val = 0x000000ff & op_src->val;
 	OPERAND_W(op_dest,val);
 #elif DATA_BYTE == 2
+		printf("DATA_BYTE == 2\n");
 	uint32_t val = 0X0000ffff & op_src->val;
 	OPERAND_W(op_dest,val);
 #else
+		printf("DATA_BYTE == 3\n");
 	OPERAND_W(op_dest, op_src->val);
 #endif
 	print_asm_template2();
