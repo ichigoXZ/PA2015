6b94c4ace107fe55b498c020a9e8079dc140e905
tracer-ics2015 <tracer@njuics.org>
2015-11-09 20:27:08 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  20:27:08 up  4:11,  3 users,  load average: 0.76, 0.70, 0.49 ca8a57eed072ca37da4dc2fa6de770dbf8f974e

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -6,12 +6,12 @@ static void do_execute () {
 #if DATA_BYTE == 2
 	//实现段寄存器
 	uint32_t val = MEM_R(cpu.esp);
-	OPERAND_W(op_dest,val);
+	OPERAND_W(op_src,val);
 	cpu.esp = cpu.esp + 2;
 #elif DATA_BYTE == 4
 	//实现段寄存器
 	uint32_t val = MEM_R(cpu.esp);
-	OPERAND_W(op_dest,val);
+	OPERAND_W(op_src,val);
 	cpu.esp = cpu.esp + 4;
 #endif
 
