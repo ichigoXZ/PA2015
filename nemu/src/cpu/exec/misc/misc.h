d1691341fcbcd0b30a4569fa67f1a3cde02464d7
tracer-ics2015 <tracer@njuics.org>
2015-11-10 18:44:24 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  18:44:24 up  3:39,  2 users,  load average: 0.73, 0.66, 0.60 19eef206a333d4f9dbb19e7bb2f7c44b5b80c042

--- nemu/src/cpu/exec/misc/misc.h
+++ nemu/src/cpu/exec/misc/misc.h
@@ -8,5 +8,13 @@ make_helper(leave);
 make_helper(ret);
 make_helper(cltd);
 make_helper(cld);
+make_helper(movsb);
+make_helper(movsw);
+make_helper(stosb);
+make_helper(stosw);
+make_helper(cmpsb);
+make_helper(cmpsw);
+make_helper(scasb);
+make_helper(scasw);
 
 #endif
