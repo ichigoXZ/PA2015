e1988af37653d2ab6ebe0be1d7270e3396ade2fd
ichigoXZ <atathunder@gmail.com>
2015-11-10 22:55:08 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  22:55:08 up  7:50,  2 users,  load average: 0.59, 0.57, 0.44 48b36f175ad46ab55ecbe784d252ec5cf0e3b097

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -3,23 +3,23 @@
 #define instr movsx
 
 static void do_execute() {
-#if DATA_BYTE == 1
-		if(op_src->val&0x80){
-			uint32_t val = 0xffffff00 | op_src->val;
-			write_operand_l(op_dest,val);
-		}
+	uint32_t val = 0;
+if (DATA_BYTE == 1){
+		if(op_src->val&0x80)
+			val = 0xffffff00 | op_src->val;
 		else 
+			val = 0x000000ff & op_src->val;
+		write_operand_l(op_dest,val);
+	}
+else if (DATA_BYTE == 2 ){
+		if(op_src->val&0x8000)
+			val = 0xffff0000 | op_src->val;
+		else
+			val = 0x0000ffff & op_src->val;
+		write_operand_l(op_dest,val);
+	}
+else
 	write_operand_l(op_dest, op_src->val);
-#elif DATA_BYTE == 2 
-			if(op_src->val&0x8000){
-			uint32_t val = 0xffff0000 | op_src->val;
-			write_operand_l(op_dest,val);
-		}
-		else 
-	write_operand_l(op_dest, op_src->val);
-#else
-	write_operand_l(op_dest, op_src->val);
-#endif
 
 	print_asm_template2();
 }
