578698f6b02e36773ff6429e1918f657efa15c82
ichigoXZ <atathunder@gmail.com>
2015-11-14 20:24:54 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  20:24:54 up  9:26,  3 users,  load average: 0.59, 0.56, 0.60 7ea1c89a902a0e7af5cbefc5129e8a75f576d94

--- nemu/src/cpu/exec/jump/call-template.h
+++ nemu/src/cpu/exec/jump/call-template.h
@@ -3,20 +3,7 @@
 #define instr call
 
 static void do_execute () {
-	if(ops_decoded.opcode == 0xff){
-#if DATA_BYTE == 2
-	cpu.esp = cpu.esp - 2;
-	//实现段寄存器
-	MEM_W(cpu.esp, cpu.eip);
-	cpu.eip = op_src->val & 0x0000ffff;
-#elif DATA_BYTE == 4
-	cpu.esp = cpu.esp - 4;
-	//实现段寄存器
-	MEM_W(cpu.esp, cpu.eip);
-	cpu.eip = op_src->val;
-#endif
-	}
-	else {
+	if(ops_decoded.opcode == 0xe8){
 #if DATA_BYTE == 2
 	cpu.esp = cpu.esp - 2;
 	//实现段寄存器
@@ -27,6 +14,19 @@ static void do_execute () {
 	//实现段寄存器
 	MEM_W(cpu.esp, cpu.eip);
 	cpu.eip = cpu.eip + op_src->val;
+#endif
+	}
+	else {
+#if DATA_BYTE == 2
+	cpu.esp = cpu.esp - 2;
+	//实现段寄存器
+	MEM_W(cpu.esp, cpu.eip);
+	cpu.eip = op_src->val & 0x0000ffff;
+#elif DATA_BYTE == 4
+	cpu.esp = cpu.esp - 4;
+	//实现段寄存器
+	MEM_W(cpu.esp, cpu.eip);
+	cpu.eip = op_src->val;
 #endif
 	}
 	print_asm_template1();
