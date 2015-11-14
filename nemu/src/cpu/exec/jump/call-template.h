6414d3ea293540fca6369638181fb8132296ea3e
ichigoXZ <atathunder@gmail.com>
2015-11-14 17:39:46 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  17:39:46 up  6:41,  3 users,  load average: 0.73, 0.71, 0.67 f5c94b895a7cfaf06bd57c0f85028600b55b236f

--- nemu/src/cpu/exec/jump/call-template.h
+++ nemu/src/cpu/exec/jump/call-template.h
@@ -3,6 +3,20 @@
 #define instr call
 
 static void do_execute () {
+	if(ops_decoded.opcode == 0xff){
+#if DATA_BYTE == 2
+	cpu.esp = cpu.esp - 2;
+	//实现段寄存器
+	MEM_W(cpu.esp, cpu.eip);
+	cpu.eip = op_src->val & 0x0000ffff;
+#elif DATA_BYTE == 4
+	cpu.esp = cpu.esp - 4;
+	//实现段寄存器
+	MEM_W(cpu.esp, cpu.eip);
+	cpu.eip = op_src->val;
+#endif
+	}
+	else {
 #if DATA_BYTE == 2
 	cpu.esp = cpu.esp - 2;
 	//实现段寄存器
@@ -14,10 +28,13 @@ static void do_execute () {
 	MEM_W(cpu.esp, cpu.eip);
 	cpu.eip = cpu.eip + op_src->val;
 #endif
-
+	}
 	print_asm_template1();
 }
 
 make_instr_helper(i) 
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+	make_instr_helper(rm)
+#endif
 
 #include "cpu/exec/template-end.h"
