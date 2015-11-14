1c3f344a7ce9bb83a58594cb247beac4c92a3282
ichigoXZ <atathunder@gmail.com>
2015-11-14 19:06:12 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  19:06:12 up  8:08,  3 users,  load average: 0.74, 0.73, 0.52 16018f72f63bdcb17d2707ff44a8857364cd65ef

--- nemu/src/cpu/exec/logic/setcc.c
+++ nemu/src/cpu/exec/logic/setcc.c
@@ -1,16 +1,16 @@
 #include "cpu/exec/helper.h"
 
 #define DATA_BYTE 1
-#include "setne-template.h"
+#include "setcc-template.h"
 #undef DATA_BYTE
 
 #define DATA_BYTE 2
-#include "setne-template.h"
+#include "setcc-template.h"
 #undef DATA_BYTE
 
 #define DATA_BYTE 4
-#include "setne-template.h"
+#include "setcc-template.h"
 #undef DATA_BYTE
 
 /* for instruction encoding overloading */
-make_helper_v(setne_rm)
+make_helper_v(setcc_rm)
