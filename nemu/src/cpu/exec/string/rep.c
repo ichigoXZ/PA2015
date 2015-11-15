31a7617099cd0d5a263ae492eda92a6113c46a2c
ichigoXZ <atathunder@gmail.com>
2015-11-15 16:50:04 +0800

    >  compile NEMU U201315092 ting Linux ele 3.13.0-65-generic #105-Ubuntu SMP Mon Sep 21 18:51:54 UTC 2015 i686 i686 i686 GNU/Linux  16:50:04 up  3:04,  2 users,  load average: 0.67, 0.56, 0.56 6fb01f7298b456bd573affb95cbb4f32a1dcecd

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -26,9 +26,9 @@ make_helper(rep) {
 			);
 			
 			/* TODO: Jump out of the while loop if necessary. */
-			if(ops_decoded.opcode==0xa6 || ops_decoded.opcode==0xa7 || ops_decoded.opcode==0xae || ops_decoded.opcode==0xaf)
+			/*if(ops_decoded.opcode==0xa6 || ops_decoded.opcode==0xa7 || ops_decoded.opcode==0xae || ops_decoded.opcode==0xaf)
 				if(cpu.ZF == 1)
-					break;
+					break;*/
 		}
 		len = 1;
 	}
