2e57ca5e419975757f23ebd0050ba18cf0d33bd0
tracer-ics2015 <tracer@njuics.org>
2015-12-28 14:52:34 +0800

    >  run U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  14:52:34 up 39 min,  2 users,  load average: 0.30, 0.55, 0.48 a135b0716726e7927e4cbbaacf0f198434c070d4

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -39,21 +39,17 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT f2F(float a) {
-	int i, uf, m, e, s, ans;
-	uf = *(int*)&a;
-//	printf("%d\n", uf);
-	m = uf & ((1 << 23) - 1);
-	e = ((uf >> 23) & ((1 << 8) - 1)) - 127;
-	s = uf >> 31;
-	ans = 1;
-	for(i = 1; i <= e + 16; ++ i) {
-		ans = (ans << 1) + ((m & (1 << 22)) >> 22);
-		if (ans < 0) return 0x80000000u;
-		m = m << 1;
-	}
-	if (s != 0) ans = (~ans) + 1;
-	return (FLOAT)(ans);
+	int f = *(int *)(&a);
+	int sign = f & 0x80000000;
+	int e = ((f & 0x7f800000)>>23) - 127;
+	int m= (f & 0x007fffff) + 0x00800000;
+	int result;
+	if(e > 7)  result =(m << (e -7));
+	else result = (m >> (7 -e));
+	if(sign) result = -result;
+	return result;
 }
+
 
 FLOAT Fabs(FLOAT a) {
 	return a>=0 ? a : -a;
