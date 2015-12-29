9e13c94910879ad8b03cabaf04036c5fe03b5b8a
tracer-ics2015 <tracer@njuics.org>
2015-12-29 08:35:14 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  08:35:14 up 27 min,  2 users,  load average: 0.13, 0.23, 0.23 8eb4182eb93c8ef0d9d5db656caf9df762b53a9d

--- nemu/src/cpu/exec/string/movs.c
+++ nemu/src/cpu/exec/string/movs.c
@@ -1,15 +1,31 @@
 #include "cpu/exec/helper.h"
 
-#define DATA_BYTE 1
-#include "movs-template.h"
-#undef DATA_BYTE
+make_helper(movs) {
+	int DATA_BYTE = 0, IncDec=0;
 
-#define DATA_BYTE 2
-#include "movs-template.h"
-#undef DATA_BYTE
+	if(ops_decoded.opcode == 0xa4) DATA_BYTE = 1;
+	else if(ops_decoded.opcode == 0xa5 && ops_decoded.is_data_size_16 == 0){
+		DATA_BYTE = 4;
+	}else DATA_BYTE = 2;
 
-#define DATA_BYTE 4
-#include "movs-template.h"
-#undef DATA_BYTE
+	IncDec = (cpu.DF == 0) ? DATA_BYTE : -DATA_BYTE;
 
-make_helper_v(movs_rm);
+	swaddr_write( cpu.edi, DATA_BYTE, swaddr_read(cpu.esi, DATA_BYTE));
+
+	reg_l(R_EDI) += IncDec;
+	reg_l(R_ESI) += IncDec;
+
+#ifdef DEBUG
+	print_asm_template1();
+#endif
+	
+	return 1;
+}
+
+make_helper(movs_b){
+	return movs(eip);
+}
+
+make_helper(movs_v){
+	return movs(eip);
+}
