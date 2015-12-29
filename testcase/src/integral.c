e24e2ce06a66cd85ebcdc7b57e87ae67d4c03307
tracer-ics2015 <tracer@njuics.org>
2015-12-29 09:17:34 +0800

    >  run U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  09:17:34 up  1:10,  2 users,  load average: 0.18, 0.56, 0.44 b106d3d6eea023804e03bc4b73cb3248eb3b8ea

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -9,13 +9,15 @@ FLOAT f(FLOAT x) {
 FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 	int k;
 	FLOAT s,h;
+	set_bp();
 	h = F_div_int((b - a), n);
+	set_bp();
 	s = F_div_int(fun(a) + fun(b), 2 );
+set_bp();
 	for(k = 1; k < n; k ++) {
 		s += fun(a + F_mul_int(h, k));
 	}
 	s = F_mul_F(s, h);
-	set_bp();
 	return s;
 }
 
