1f6fb8f69fde8224d684ef333de397b88424ffe7
tracer-ics2015 <tracer@njuics.org>
2015-12-29 22:50:33 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  22:50:33 up 14:43,  3 users,  load average: 0.51, 0.37, 0.29 8efc27b669664892e525068cd5bf04c5c54555da

--- nemu/src/cpu/exec/jump/call-template.h
+++ nemu/src/cpu/exec/jump/call-template.h
@@ -3,7 +3,6 @@
 #define instr call
 
 static void do_execute () {
-	if(ops_decoded.opcode == 0xe8){
 #if DATA_BYTE == 2
 	cpu.esp = cpu.esp - 2;
 	//实现段寄存器
@@ -15,26 +14,25 @@ static void do_execute () {
 	MEM_W(cpu.esp, cpu.eip);
 	cpu.eip = cpu.eip + op_src->val;
 #endif
-	}
-	else {
-#if DATA_BYTE == 2
-	cpu.esp = cpu.esp - 2;
-	//实现段寄存器
-	MEM_W(cpu.esp, cpu.eip);
-	cpu.eip = (op_src->val & 0x0000ffff) -2;
-#elif DATA_BYTE == 4
-	cpu.esp = cpu.esp - 4;
-	//实现段寄存器
-	MEM_W(cpu.esp,cpu.eip);
-	cpu.eip += op_src->val ;
-#endif
-	}
 	print_asm_template1();
 }
 
 make_instr_helper(i) 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
-	make_instr_helper(rm)
+make_helper(concat(call_rm_, SUFFIX)){
+	concat(decode_rm_,SUFFIX)(cpu.eip+1);
+	if(2 == DATA_BYTE){
+		cpu.esp =cpu.esp - 2;
+		MEM_W(cpu.esp, (uint16_t)(cpu.eip& 0x0000ffff));
+		cpu.eip = ( op_src->val)&0x0000ffff;
+	}
+	else if(4 == DATA_BYTE){
+		cpu.esp = cpu.esp-4;
+		MEM_W(cpu.esp,  cpu.eip);
+		cpu.eip =  op_src->val;
+	}
+	return 0;	
+}
 #endif
 
 #include "cpu/exec/template-end.h"
