606bf22774ea838e86bacf775a43fdc3aff8561d
tracer-ics2015 <tracer@njuics.org>
2015-12-30 22:03:29 +0800

    >  compile kernel U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  22:03:29 up  8:01,  3 users,  load average: 0.55, 0.45, 0.40 90b10cfd66b5a58319d71a6431e3b9b5e3d5c47f

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -37,7 +37,7 @@ uint32_t loader() {
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	panic("please implement me");
+	//panic("please implement me");
 	int i;
 	for(i = 0; i <=  elf->e_phnum; i++ ) {
 		/* Scan the program header table, load each segment into memory */
@@ -47,12 +47,13 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			ramdisk_write((uint8_t*)(ph -> p_vaddr), ph->p_offset, ph->p_filesz);
+			uint32_t addr=mm_malloc(ph->p_vaddr, ph->p_memsz);
+			ramdisk_read((void*)addr, ph->p_offset, ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			  memset((void*)(ph -> p_vaddr+ ph -> p_filesz), 0, ph -> p_memsz - ph -> p_filesz);
+			 memset((void*)addr+ph->p_filesz, 0, ph->p_memsz-ph->p_filesz); 
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
