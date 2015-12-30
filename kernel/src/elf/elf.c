f22a4360259e7122f2e0b3f3c5955c0b5031b041
tracer-ics2015 <tracer@njuics.org>
2015-12-30 18:35:57 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  18:35:57 up  4:34,  3 users,  load average: 0.25, 0.22, 0.23 de4f4f89596c720dd79f0e2aad9f957e577d1b

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -32,20 +32,22 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0x7f454c46;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
 	//panic("please implement me");
-	for(; true; ) {
+	int i;
+	for(i = 0; i <=  elf->e_phnum; i++ ) {
 		/* Scan the program header table, load each segment into memory */
+		ph = elf + elf -> e_ehsize + i * elf -> e_phentsize; 
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			//  ramdisk_write(buf+ph->p_offset,ph->p_vaddr,ph->p_memsz);
+			ramdisk_write(, ph->);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
