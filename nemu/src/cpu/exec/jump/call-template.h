99c06b75c8e521e9892196275c724711f3a5b63d
tracer-ics2015 <tracer@njuics.org>
2015-12-30 09:48:11 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  09:48:11 up 40 min,  2 users,  load average: 0.12, 0.12, 0.19 9872e1a2aa0530fefbdde1be370565372ebdcb33

--- nemu/src/cpu/exec/jump/call-template.h
+++ nemu/src/cpu/exec/jump/call-template.h
@@ -5,16 +5,14 @@
 
 static void do_execute (){
 	int len=concat(decode_i_, SUFFIX)(cpu.eip + 1) + 1;
-	if(2 == DATA_BYTE){
-		cpu.esp =cpu.esp - 2;
-		MEM_W(cpu.esp, (uint16_t)((cpu.eip) & 0x0000ffff));
-
-		cpu.eip = (cpu.eip + op_src->val)&0x0000ffff;
-	}
-	else if(4 == DATA_BYTE){
-		cpu.esp = cpu.esp-4;
+	cpu.esp -= DATA_BYTE;
+	if(4 == DATA_BYTE){
 		MEM_W(cpu.esp,  cpu.eip + len);
 		cpu.eip = cpu.eip+ op_src->val;
+	}
+	else if(2 == DATA_BYTE){
+		MEM_W(cpu.esp, (uint16_t)((cpu.eip + len) & 0x0000ffff));
+		cpu.eip = (cpu.eip + op_src->val)&0x0000ffff;
 	}
 	print_asm_template1();
 }
@@ -23,13 +21,12 @@ make_instr_helper(i)
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_helper(concat(call_rm_, SUFFIX)){
 	int len=concat(decode_rm_, SUFFIX)(eip + 1) + 1;
+	cpu.esp -= DATA_BYTE;
 	if(4 == DATA_BYTE){
-		cpu.esp = cpu.esp-4;
-		MEM_W(cpu.esp,  cpu.eip);
+		MEM_W(cpu.esp,  cpu.eip + len);
 		cpu.eip =  op_src->val;
 	}
 	else if(2 == DATA_BYTE){
-		cpu.esp =cpu.esp - 2;
 		MEM_W(cpu.esp, (uint16_t)((cpu.eip+len)& 0x0000ffff));
 		cpu.eip = ( op_src->val)&0x0000ffff;
 	}
