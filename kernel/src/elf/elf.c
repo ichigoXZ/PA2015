048808e8675e10e47bb20ed0662b0114514ad191
tracer-ics2015 <tracer@njuics.org>
2015-12-30 21:55:34 +0800

    >  compile kernel U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  21:55:34 up  7:53,  3 users,  load average: 0.43, 0.33, 0.34 6c4ecddd96cc5e21a43fd917182875656429985

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -35,9 +35,9 @@ uint32_t loader() {
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
-	
+
 	/* Load each program segment */
-	//panic("please implement me");
+	panic("please implement me");
 	int i;
 	for(i = 0; i <=  elf->e_phnum; i++ ) {
 		/* Scan the program header table, load each segment into memory */
