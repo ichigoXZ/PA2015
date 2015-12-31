a64023611fd50b9db745f3d78bd450c15935732a
tracer-ics2015 <tracer@njuics.org>
2015-12-31 16:38:47 +0800

    >  compile kernel U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  16:38:47 up  3:50,  3 users,  load average: 0.28, 0.31, 0.27 c98549117808510fb630a46493b3498bf2985ecf

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -47,7 +47,6 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			uint32_t addr=mm_malloc(ph->p_vaddr, ph->p_memsz);
 			ramdisk_read((void*)ph->p_vaddr, ph->p_offset, ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
