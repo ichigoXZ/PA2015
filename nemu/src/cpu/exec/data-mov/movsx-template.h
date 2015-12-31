f3a66420a11bdd72f0293cdfb818fc11305c2796
ichigoXZ <atathunder@gmail.com>
2015-12-31 23:07:00 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  23:07:00 up 10:18,  3 users,  load average: 0.24, 0.37, 0.45 779abb63bcdcb9a4261cbf1c1d0feddee343cfc5

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -1,4 +1,4 @@
-#include "cpu/exec/template-start.h"
+/*#include "cpu/exec/template-start.h"
 
 #define instr movsx
 
@@ -21,6 +21,30 @@ else if (DATA_BYTE == 2 ){
 else
 	write_operand_l(op_dest, op_src->val);
 
+	print_asm_template2();
+}
+
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
+*/
+#include "cpu/exec/template-start.h"
+#define instr movsx
+
+static void do_execute (){
+	if(DATA_BYTE ==2){
+		int16_t signExtend = (int16_t)((int8_t) op_src->val);
+		OPERAND_W(op_dest , signExtend);
+	}else{
+		if(ops_decoded.opcode == (0xbf | 0x100)){
+			int32_t signExtend = (int32_t)((int16_t) op_src->val);
+			OPERAND_W(op_dest , signExtend);
+		}else{
+			int32_t signExtend = (int32_t)((int8_t) op_src->val);
+			OPERAND_W(op_dest , signExtend);
+		}
+	}
+	
 	print_asm_template2();
 }
 
