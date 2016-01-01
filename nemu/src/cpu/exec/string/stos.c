ac05c0f3eda30dbf933f6a82fbe55055aae1b943
ichigoXZ <atathunder@gmail.com>
2016-01-01 14:29:20 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  14:29:20 up  4:08,  2 users,  load average: 0.37, 0.44, 0.42 8f7d59e8280b0975f6ed4be6cefa8c37729e9c88

--- nemu/src/cpu/exec/string/stos.c
+++ nemu/src/cpu/exec/string/stos.c
@@ -1,22 +1,23 @@
 #include "cpu/exec/helper.h"
 
 make_helper(stos) {
-	int data_byte = 0;
+	int data_byte = 0, IncDec = 0;
 
 	if(ops_decoded.opcode == 0xaa) data_byte = 1;
 	else if(ops_decoded.opcode == 0xab && ops_decoded.is_data_size_16 == 0){
 		data_byte = 4;
 	}else data_byte = 2;
 	
+	IncDec = (cpu.DF == 0)?data_byte:-data_byte;
 	if(data_byte == 1)
 		swaddr_write(cpu.edi,1,cpu.gpr[R_EAX]._8[0]);
 	else if (data_byte == 2)
 		swaddr_write(cpu.edi,2,cpu.gpr[R_AX]._16);
 	else
 		swaddr_write(cpu.edi,4,cpu.eax);
-
-	cpu.esi -= data_byte;
-	cpu.edi -= data_byte;
+	
+	cpu.esi += IncDec;
+	cpu.edi += IncDec;
 
 #ifdef DEBUG
 	print_asm_template1();
