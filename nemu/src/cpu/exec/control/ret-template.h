a8f59c8ef3f7c9dc47faf09c64c1dd97bcb3e47d
ichigoXZ <atathunder@gmail.com>
2016-01-01 12:56:42 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  12:56:42 up  2:35,  2 users,  load average: 0.08, 0.16, 0.15 83fe8a9afdb930d6d72e50466d29675de3fee70d

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -3,15 +3,12 @@
 #define instr ret
 
 static void do_execute () {
+	cpu.eip = MEM_R(cpu.esp);
+	cpu.esp += DATA_BYTE;
 #if DATA_BYTE == 2
 	cpu.eip = cpu.eip & 0x0000ffff;
-	cpu.eip = MEM_R(cpu.esp);
-	cpu.esp += DATA_BYTE;
-#elif DATA_BYTE == 4
-	cpu.eip = MEM_R(cpu.esp);
-	cpu.esp += DATA_BYTE;
 #endif
-	cpu.esp += op_src->val & 0x0000ffff;
+	cpu.esp += op_src->val;
 
 	print_asm_template1();
 }
