eaaf9fab0102ebeed1a0154e754503246a585fa6
tracer-ics2015 <tracer@njuics.org>
2016-01-01 14:14:31 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  14:14:31 up  3:53,  2 users,  load average: 0.67, 0.46, 0.41 6656d60215df2f26a59f20e438b4303b59e42585

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -1,16 +1,16 @@
-#include "cpu/exec/template-start.h"
+/*#include "cpu/exec/template-start.h"
 
 #define instr movzx
 
 static void do_execute() {
 #if DATA_BYTE == 1	
 	uint32_t val = 0x000000ff & op_src->val;
-	OPERAND_W(op_dest,val);
+	write_operand_l(op_dest,val);
 #elif DATA_BYTE == 2
 	uint32_t val = 0X0000ffff & op_src->val;
-	OPERAND_W(op_dest,val);
+	write_operand_l(op_dest,val);
 #else
-	OPERAND_W(op_dest,op_src->val);
+	write_operand_l(op_dest,op_src->val);
 #endif
 	print_asm_template2();
 }
@@ -18,4 +18,26 @@ static void do_execute() {
 make_instr_helper(rm2r)
 
 #include "cpu/exec/template-end.h"
+*/
+#include "cpu/exec/template-start.h"
+#define instr movzx
 
+static void do_execute (){
+	uint32_t zeroExtend = 0;
+	if(DATA_BYTE == 2)
+		zeroExtend = 0x000000ff & op_src->val;
+	else{
+		if(ops_decoded.opcode == (0xb6 | 0x100)){
+			zeroExtend = 0x000000ff & op_src->val;
+		}else{
+			zeroExtend = 0x0000ffff & op_src->val;
+		}
+	}
+		
+	write_operand_l(op_dest , zeroExtend);
+	print_asm_template2();
+}
+
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
