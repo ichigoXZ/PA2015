2da408b2ac0af11092cbfad75316d85dd136ddc5
ichigoXZ <atathunder@gmail.com>
2016-01-01 13:57:41 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  13:57:41 up  3:36,  2 users,  load average: 0.27, 0.30, 0.37 75c9d81bd42eab93d609130280adbe147eaf48a9

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -3,11 +3,10 @@
 
 
 static void do_execute (){
-	//printf("eip: 0x%x\n",cpu.eip );
-	int len=concat(decode_i_, SUFFIX)(cpu.eip + 1) +1;
+	printf("eip: 0x%x\n",cpu.eip );
+	int len=concat(decode_i_, SUFFIX)(cpu.eip + 1) ;
 	//printf("len:%d\n",len );
 	cpu.esp -= DATA_BYTE;
-	printf("cpu.esp:0x%x\n",cpu.esp );
 	if(4 == DATA_BYTE){
 		MEM_W(cpu.esp,  cpu.eip + len);
 		cpu.eip = cpu.eip+ op_src->val;
