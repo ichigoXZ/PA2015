cc0f799d876126edeb4b7b6f8fde3da732e6bbb7
tracer-ics2015 <tracer@njuics.org>
2016-01-01 13:16:28 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  13:16:28 up  2:55,  2 users,  load average: 0.56, 0.99, 0.63 dd2c4a19eb64e5a61c023959995afb38ce3766c

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -2,20 +2,19 @@
 
 #define instr ret
 
-static void do_execute () {
-	cpu.eip = MEM_R(cpu.esp);
-	cpu.esp += DATA_BYTE;
-#if DATA_BYTE == 2
-	cpu.eip = cpu.eip & 0x0000ffff;
-#endif
-	printf("cpu.esp: 0x%x\n",cpu.esp );
-	cpu.esp += (op_src->val & 0x0000ffff);
-	printf("op_src:0x%x\n",op_src->val);
-	printf("cpu.esp: 0x%x\n",cpu.esp );
-
-	print_asm_template1();
+make_helper(concat(ret_i_, SUFFIX)){
+	concat(decode_i_, SUFFIX)(cpu.eip + 1);
+	if(ops_decoded.opcode == 0xc2){
+		if(DATA_BYTE == 2){
+			cpu.eip = MEM_R(cpu.esp);
+			cpu.esp += 2;
+			cpu.eip &= 0x0000ffff;
+		}else{
+			cpu.eip = MEM_R(cpu.esp);
+			cpu.esp += 4;
+		}
+	}
+	return 0;
 }
-
-make_instr_helper(i)
 
 #include "cpu/exec/template-end.h"
