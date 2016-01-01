88e8b74def6dd39ff3f67ec833234bb028fb871d
tracer-ics2015 <tracer@njuics.org>
2016-01-01 13:41:28 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  13:41:28 up  3:20,  2 users,  load average: 0.40, 0.44, 0.49 515cabc1b87e64eec2929c33714c6cc68e235cff

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -2,15 +2,15 @@
 
 #define instr ret
 
-static void do_execute () {
-	cpu.eip = MEM_R(cpu.esp);
-	cpu.esp += DATA_BYTE;
-#if DATA_BYTE == 2
-	cpu.eip = cpu.eip & 0x0000ffff;
-#endif
-	cpu.esp += op_src->val;
-
-	print_asm_template1();
+static void do_execute() {
+	uint32_t ans = swaddr_read(cpu.esp, 4);
+	cpu.esp += 4 + op_src->val;
+	cpu.eip = ans;
+	if(op_src->val == 0) print_asm("ret"); 
+	else {
+		cpu.eip -= 2;
+		print_asm_template1();
+	}
 }
 
 make_instr_helper(i)
