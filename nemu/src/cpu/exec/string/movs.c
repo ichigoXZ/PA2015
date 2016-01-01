adba76f055f2792ac19a57e8e156e49f299e10a6
tracer-ics2015 <tracer@njuics.org>
2016-01-01 11:24:20 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-74-generic #118-Ubuntu SMP Thu Dec 17 22:52:02 UTC 2015 i686 i686 i686 GNU/Linux  11:24:20 up  1:03,  2 users,  load average: 0.33, 0.25, 0.23 f115fbd42d1a1f1985caf438abd42794e894af

--- nemu/src/cpu/exec/string/movs.c
+++ nemu/src/cpu/exec/string/movs.c
@@ -1,16 +1,17 @@
+
 #include "cpu/exec/helper.h"
 
 make_helper(movs) {
-	int data_byte = 0, IncDec=0;
-	printf("yes!\n");
-	if(ops_decoded.opcode == 0xa4) data_byte = 1;
+	int DATA_BYTE = 0, IncDec=0;
+
+	if(ops_decoded.opcode == 0xa4) DATA_BYTE = 1;
 	else if(ops_decoded.opcode == 0xa5 && ops_decoded.is_data_size_16 == 0){
-		data_byte = 4;
-	}else data_byte = 2;
+		DATA_BYTE = 4;
+	}else DATA_BYTE = 2;
 
-	IncDec = (cpu.DF == 0) ? data_byte : -data_byte;
+	IncDec = (cpu.DF == 0) ? DATA_BYTE : -DATA_BYTE;
 
-	swaddr_write( cpu.edi, data_byte, swaddr_read(cpu.esi, data_byte));
+	swaddr_write( cpu.edi, DATA_BYTE, swaddr_read(cpu.esi, DATA_BYTE));
 
 	cpu.edi += IncDec;
 	cpu.esi += IncDec;
