41ab8a37815f88f6c9e8f8e327510013b61f4414
ichigoXZ <atathunder@gmail.com>
2016-01-25 16:09:54 +0800

    >  compile NEMU U201315092 tan Linux ichigoXY 3.13.0-76-generic #120-Ubuntu SMP Mon Jan 18 15:58:41 UTC 2016 i686 i686 i686 GNU/Linux  16:09:54 up  6:04,  2 users,  load average: 0.08, 0.16, 0.21 3729115db16841231ee59a6bf2cabf7166cf080

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -10,6 +10,7 @@ void load_elf_tables(int, char *[]);
 void init_regex();
 void init_wp_list();
 void init_ddr3();
+void init_cache();
 
 FILE *log_fp = NULL;
 
@@ -93,4 +94,7 @@ void restart() {
 
 	/* Initialize DRAM. */
 	init_ddr3();
+
+	/* Initialize cache*/
+	init_cache();
 }
